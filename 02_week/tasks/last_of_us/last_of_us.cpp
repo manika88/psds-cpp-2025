0f0f08926c3b76c9b3613358ac9210a78268d7a0
manika88 <gasilovamarij184@gmail.com>
2025-12-09 23:16:33 +0500

    add (solution): add last_of_us task

--- 02_week/tasks/last_of_us/last_of_us.cpp
+++ 02_week/tasks/last_of_us/last_of_us.cpp
@@ -1,6 +1,14 @@
 #include <stdexcept>
 
 
-/* return_type */ FindLastElement(/* ptr_type */ begin, /* ptr_type */ end, /* func_type */ predicate) {
-    throw std::runtime_error{"Not implemented"};
+int* FindLastElement(int* begin,int* end, bool (*predicate)(int)) {
+    if (begin == nullptr || end == nullptr || begin > end) {
+        return end;
+    }
+    for (int* p = end - 1; p >= begin; --p) {
+        if (predicate(*p)) {
+            return p;
+        }
+    }
+    return end;
 }
