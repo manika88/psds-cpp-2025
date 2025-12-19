ec5ca799b9eda22355208890a89dc32d0bdc8728
manika88 <gasilovamarij184@gmail.com>
2025-12-20 00:04:34 +0500

    add (solution): add find_all task

--- 03_week/tasks/find_all/find_all.cpp
+++ 03_week/tasks/find_all/find_all.cpp
@@ -1,6 +1,15 @@
 #include <stdexcept>
+#include <vector>
 
 
-/* return_type */ FindAll(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+std::vector<size_t> FindAll(const std::vector<int>& container, bool (*predicate)(int)) {
+    std::vector<size_t> cont2;
+
+    for (size_t i = 0; i < container.size(); ++i) {
+        if (predicate(container[i])) {
+            cont2.push_back(i);
+        }
+    }
+
+    return cont2;
 }
