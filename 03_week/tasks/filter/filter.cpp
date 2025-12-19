3d9c0827cfdc2ba99ed7c749a8a56dd123c625fb
manika88 <gasilovamarij184@gmail.com>
2025-12-20 00:11:42 +0500

    add (solution): add filter task

--- 03_week/tasks/filter/filter.cpp
+++ 03_week/tasks/filter/filter.cpp
@@ -1,6 +1,14 @@
 #include <stdexcept>
+#include <vector>
 
 
-/* return_type */ Filter(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+void Filter(std::vector<int>& v, bool (*predicate)(int)) {
+    size_t ind = 0; 
+    for (size_t i = 0; i < v.size(); ++i) {
+        if (predicate(v[i])) {
+            v[ind] = v[i];
+            ++ind;
+        }
+    }
+    v.resize(ind);
 }
